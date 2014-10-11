6a5795fc3f8c48df41fdc9b72a809ffe1ebde9fe
Florian Baumann <flo@noqqe.de>
2014-10-11 20:54:07 +0200

    added dates and functions

--- maildirstats.py
+++ maildirstats.py
@@ -4,27 +4,60 @@ import os
 import sys
 import time
 import mailbox
+import operator
 from email.header import decode_header
+import email.utils
 
-mymaildir="/home/noqqe/Maildir/INBOX/"
+# Settings
+box="/home/noqqe/Maildir/INBOX/"
+box = mailbox.Maildir(box, factory=mailbox.MaildirMessage)
 people = dict()
+mailYear = dict()
+mailMonth = dict()
+mailDay= dict()
 
-box = mailbox.Maildir(mymaildir, factory=mailbox.MaildirMessage)
-
+# Functions (low level)
 def getHeader(msg,header):
    m = msg.get(header, 'empty')
    m = decode_header(m)
    m = m[0][0] # transform list(tuple()) into str()
    return m
 
-
-
-for msg in box:
+# Functions (high level)
+def senderList(msg):
     f = getHeader(msg,"From")
     if f in people:
         people[f] += 1
     else:
         people[f] = 1
 
-print people 
+def mailDate(msg):
+    date = getHeader(msg,"Date")
+    date = email.utils.parsedate(date)
+    full = time.strftime("%F %H:%M:%S", date)
+    y = time.strftime("%Y", date)
+    if y in mailYear:
+        mailYear[y] += 1
+    else:
+        mailYear[y] = 1
+    m = time.strftime("%m", date)
+    if m in mailMonth:
+        mailMonth[m] += 1
+    else:
+        mailMonth[m] = 1
+    d = time.strftime("%d", date)
+    if d in mailDay:
+        mailDay[d] += 1
+    else:
+        mailDay[d] = 1
+ 
+# Runtime
+for msg in box:
+    #senderList(msg)
+    mailDate(msg)
+
+#print sorted(people.items(), key=operator.itemgetter(1)) 
+print sorted(mailYear.items(), key=operator.itemgetter(1)) 
+print sorted(mailMonth.items(), key=operator.itemgetter(1)) 
+print sorted(mailDay.items(), key=operator.itemgetter(1)) 
 
