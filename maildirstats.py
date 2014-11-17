855be94e1e49f5429d49e1116f42e4375c3517d7
Florian Baumann <flo@noqqe.de>
2014-11-17 15:07:34 +0100

    weekday, hour, minute feature

--- maildirstats.py
+++ maildirstats.py
@@ -18,7 +18,10 @@ dirs = [k for k in os.listdir(boxesdir) if re.match(blacklist,k) is None ]
 people = dict()
 mailYear = dict()
 mailMonth = dict()
-mailDay= dict()
+mailDay = dict()
+mailWeekday = dict()
+mailHour = dict()
+mailMinute = dict()
 
 # Functions (low level)
 def getHeader(msg,header):
@@ -43,16 +46,19 @@ def mailDate(msg):
     date = getHeader(msg,"Date")
     try:
         date = email.utils.parsedate(date)
-        full = time.strftime("%F %H:%M:%S", date)
+        date = time.localtime(time.mktime(date))
         scoreMap(time.strftime("%Y", date),mailYear)
         scoreMap(time.strftime("%m", date),mailMonth)
         scoreMap(time.strftime("%d", date),mailDay)
+        scoreMap(time.strftime("%A", date),mailWeekday)
+        scoreMap(time.strftime("%H", date),mailHour)
+        scoreMap(time.strftime("%M", date),mailMinute)
     except:
-        pass 
- 
+        pass
+
 # Runtime
 for box in dirs:
-    try: 
+    try:
         maildir = mailbox.Maildir(boxesdir + box, factory=mailbox.MaildirMessage)
         for msg in maildir:
             senderList(msg)
@@ -62,21 +68,36 @@ for box in dirs:
 
 print "### People"
 for x in sorted(people.items(), key=operator.itemgetter(1)):
-    print str(x[1])+": "+str(x[0])  
+    print str(x[1])+": "+str(x[0])
 print ""
 
 print "### Year"
 for x in sorted(mailYear.items(), key=operator.itemgetter(0)):
-    print str(x[0])+": "+str(x[1])  
+    print str(x[0])+": "+str(x[1])
 print ""
 
 print "### Month"
 for x in sorted(mailMonth.items(), key=operator.itemgetter(0)):
-    print str(x[0])+": "+str(x[1])  
+    print str(x[0])+": "+str(x[1])
 print ""
 
 print "### Day"
 for x in sorted(mailDay.items(), key=operator.itemgetter(0)):
-    print str(x[0])+": "+str(x[1])  
+    print str(x[0])+": "+str(x[1])
+print ""
+
+print "### Weekday"
+for x in mailWeekday.items():
+    print str(x[0])+": "+str(x[1])
+print ""
+
+print "### Hour"
+for x in sorted(mailHour.items(), key=operator.itemgetter(0)):
+    print str(x[0])+": "+str(x[1])
+print ""
+
+print "### Minute"
+for x in sorted(mailMinute.items(), key=operator.itemgetter(0)):
+    print str(x[0])+": "+str(x[1])
 print ""
 
